e7eb5456160c0c885da434b5a0aa1f18911307c6
Sanqiang Zhao <zhaosanqiang916@gmail.com>
2015-11-03 22:48:00 -0500

    fix generate matrix

--- generate_triple.m
+++ generate_triple.m
@@ -1,4 +1,14 @@
 function tripe = generate_triple(m)
 
+n_aspect = size(m,1);
+n_reviewer = size(m,2);
+
+mean_aspect = zeros(n_aspect,1);
+zero_aspect = zeros(n_aspect,1);
+
+for idx = 1:n_aspect
+   mean_aspect(idx) = mean(m(idx,m(idx,:) ~= 0));
+   zero_aspect(idx) = length(m(idx,m(idx,:) == 0)) / n_reviewer;
+end
 
 end
